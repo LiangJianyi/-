d33392c0d9c42ee3f696aeb05d2a70cc628574cb
梁建溢 <liangjianyi.com@outlook.com>
2018-07-17 07:02:58 -0400

    Changesets-01

--- 风骚哥签到日期计算器/MainPage.xaml.cs
+++ 风骚哥签到日期计算器/MainPage.xaml.cs
@@ -13,18 +13,27 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+namespace 风骚哥签到日期计算器 {
+	using Debug = System.Diagnostics.Debug;
+	public sealed partial class MainPage : Page {
+		public MainPage() {
+			this.InitializeComponent();
+		}
 
-namespace 风骚哥签到日期计算器
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+		private void CalendarDatePicker_Previous_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args) {
+			var calendar = sender as CalendarDatePicker;
+			Debug.WriteLine($"Previous: {calendar.Date.Value.Month}/{calendar.Date.Value.Day}/{calendar.Date.Value.Year}");
+		}
+
+		private void CalendarDatePicker_Today_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args) {
+			var calendar = sender as CalendarDatePicker;
+			var offset = calendar.Date - CalendarDatePicker_Previous.Date;
+			if (CalendarDatePicker_Previous.Date != null) {
+				Debug.WriteLine($"风骚哥本次签到距离上次签到已过了: {offset.Value.Days}天");
+			}
+			else {
+				Debug.WriteLine("error");
+			}
+		}
+	}
 }
